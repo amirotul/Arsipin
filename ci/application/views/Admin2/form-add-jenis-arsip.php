0a221c1af88a3e884953889a5944df9a7dbbd329
Makhmuda Nurul Khasana <makhmudanurul2000@gmail.com>
2021-10-30 20:43:37 +0700

    jumat

--- ci/application/views/Admin2/form-add-jenis-arsip.php
+++ ci/application/views/Admin2/form-add-jenis-arsip.php
@@ -1,18 +1,18 @@
 <form class="user" action="<?php echo base_url('Jenis_Arsip/input');?>" method="post">
-<div class="page-content-wrapper">
-    <div class="container-fluid">
-        <!-- end page title end breadcrumb -->
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="page-title-box">
-                    <h4 class="page-title">Add Jenis Arsip</h4>
+    <div class="page-content-wrapper">
+        <div class="container-fluid">
+            <!-- end page title end breadcrumb -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <h4 class="page-title">Add Jenis Arsip</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="card m-b-30">
-                    <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card m-b-30">
+                        <div class="card-body">
                             <div class="form-group row">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Jenis Arsip</label>
                                 <div class="col-sm-5">
@@ -26,39 +26,16 @@
                                             <div class="input-group-append">
                                             </form></hr>
                                             <a><input type="submit" class="btn btn-primary btn-sm" name="submit" value="Simpan"></a>
-                                            </div>
-                                            
-                                        </div> &nbsp &nbsp
-                                        <div class="input-group-append">
+                                        </div>
+                                        
+                                    </div> &nbsp &nbsp
+                                    <div class="input-group-append">
 
-                                            <a href=<?php echo base_url('Jenis_Arsip')?>>
-                                                <button class="btn btn-danger btn-sm" type="button">Batal</button></a>
-                                            </div>
+                                        <a href=<?php echo base_url('Jenis_Arsip')?>>
+                                            <button class="btn btn-danger btn-sm" type="button">Batal</button></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div> <!-- end col -->
-                </div> <!-- end row -->
-
-
-            </div><!-- container -->
-
-        </div> <!-- Page content Wrapper -->
-
-    </div><!-- container -->
-
-</div> <!-- Page content Wrapper -->
-
-</div> <!-- content -->
-
-<footer class="footer">
-    © 2018 - 2020 Dashor by Themesdesign.
-</footer>
-
-</div>
-<!-- End Right content here -->
-
-</div>
-    <!-- END wrapper -->
+                    </div>
