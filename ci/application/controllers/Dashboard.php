2240dd5aff7962b4e20477481008b35223f4f653
amirotul <amirotulkotimah@gmail.com>
2021-11-04 11:20:12 +0700

    tes

--- ci/application/controllers/Dashboard.php
+++ ci/application/controllers/Dashboard.php
@@ -4,11 +4,16 @@ class Dashboard extends CI_Controller{ //membuat controller Mahasiswa
 	function __construct(){
 		parent:: __construct();
 		$this->load->model('Admin_model');
+		$this->load->model('Data_arsip_model');
+		$this->load->model('Datadivisi_model');
 		//untuk mengakses file model 'Mahasiswa_model'
 	}
 
 	public function index(){ //function untuk menampilkan halaman awal yang ditampilkan
 		$data['user'] = $this->Admin_model->getAll()->result();
+		$data['total_data_arsip'] = $this->Data_arsip_model-> count_all_arsip();
+		$data['total_data_divisi'] = $this->Datadivisi_model-> count_all_divisi();
+
 		$this->template->views('Admin2/dashboard',$data);
 			//untuk mengakses file views 'crud/home_mahasiswa' pada halaman template
 	}
