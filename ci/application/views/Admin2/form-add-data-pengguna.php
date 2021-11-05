d2a240a426268d271f73af3282abe037389b8568
salmabinarabdul <65655844+salmabinarabdul@users.noreply.github.com>
2021-11-05 10:04:56 +0700

    pengguna

--- ci/application/views/Admin2/form-add-data-pengguna.php
+++ ci/application/views/Admin2/form-add-data-pengguna.php
@@ -29,10 +29,10 @@
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="example-date-input" class="col-sm-2 col-form-label">Jenis Divisi</label>
+                                        <label for="example-date-input" class="col-sm-2 col-form-label">Role</label>
                                         <div class="col-sm-5">
                                             <select class="form-control" id="master_user" name="master_user" required="">
-                                                <option value="" hidden disabled selected>Pilih Jenis Divisi</option>
+                                                <option value="" hidden disabled selected>Pilih Role</option>
                                                 <?php foreach ($role as $item) : ?>
                                                     <option value="<?= $item->id_role ?>"><?= $item->nama_role ?></option>
                                                 <?php endforeach ?>
