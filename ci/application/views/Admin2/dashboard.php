e5adebc0b4c81f09bc0777daa14ee7471ea43c14
Makhmuda Nurul Khasana <makhmudanurul2000@gmail.com>
2021-12-07 15:46:28 +0700

    07 12

--- ci/application/views/Admin2/dashboard.php
+++ ci/application/views/Admin2/dashboard.php
@@ -154,7 +154,7 @@
                         type: 'areaspline'
                       },
                       title: {
-                        text: 'Jumlah dokumen pertahun'
+                        text: 'Jumlah Dokumen Per-tahun'
                       },
 
 
@@ -214,8 +214,7 @@
 
         data: [ 
         <?php echo json_encode($data_dis_pertahun_21); ?>,
-        <?php echo json_encode($data_dis_pertahun_22);
-        ?>,
+        <?php echo json_encode($data_dis_pertahun_22); ?>,
         <?php echo json_encode($data_dis_pertahun_23); ?>,
         <?php echo json_encode($data_dis_pertahun_24); ?>,
         <?php echo json_encode($data_dis_pertahun_25); ?> 
@@ -228,274 +227,49 @@
   // Create the chart
   Highcharts.chart('container', {
     chart: {
-      type: 'pie'
-    },
-    title: {
-      text: 'Disposisi'
-    },
-
-    accessibility: {
-      announceNewData: {
-        enabled: true
-      },
-      point: {
-        valueSuffix: '%'
-      }
-    },
-
-    credits: {
-      enabled: false
-    },
-
-    plotOptions: {
-      series: {
-        name: "Surat Masuk",
-        dataLabels: {
-          enabled: false,
-          format: '{point.name}: {point.y}'
-        }
-      }
-    },
-
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y} Dokumen<br/>'
-    },
-
-    series: [
-    {
-      name: "Browsers",
-      colorByPoint: true,
-      data: [
-      {
-        y: 62.74
-      },
-      {
-        y: 10.57
-      }
-      ]
+     plotBackgroundColor: null,
+     plotBorderWidth: null,
+     plotShadow: false,
+     type: 'pie'
+   },
+   title: {
+    text: 'Disposisi Surat Masuk'
+  },
+  tooltip: {
+    pointFormat: '<b>{point.percentage:.1f}%</b>'
+  },
+  credits: {
+    enabled: false
+  },
+  accessibility: {
+    point: {
+      valueSuffix: '%'
     }
-    ],
-    drilldown: {
-      series: [{
-        name: "Chrome",
-        id: "Chrome",
-        data: [
-        [
-        "v65.0",
-        0.1
-        ],
-        [
-        "v64.0",
-        1.3
-        ],
-        [
-        "v63.0",
-        53.02
-        ],
-        [
-        "v62.0",
-        1.4
-        ],
-        [
-        "v61.0",
-        0.88
-        ],
-        [
-        "v60.0",
-        0.56
-        ],
-        [
-        "v59.0",
-        0.45
-        ],
-        [
-        "v58.0",
-        0.49
-        ],
-        [
-        "v57.0",
-        0.32
-        ],
-        [
-        "v56.0",
-        0.29
-        ],
-        [
-        "v55.0",
-        0.79
-        ],
-        [
-        "v54.0",
-        0.18
-        ],
-        [
-        "v51.0",
-        0.13
-        ],
-        [
-        "v49.0",
-        2.16
-        ],
-        [
-        "v48.0",
-        0.13
-        ],
-        [
-        "v47.0",
-        0.11
-        ],
-        [
-        "v43.0",
-        0.17
-        ],
-        [
-        "v29.0",
-        0.26
-        ]
-        ]
-      },
-      {
-        name: "Firefox",
-        id: "Firefox",
-        data: [
-        [
-        "v58.0",
-        1.02
-        ],
-        [
-        "v57.0",
-        7.36
-        ],
-        [
-        "v56.0",
-        0.35
-        ],
-        [
-        "v55.0",
-        0.11
-        ],
-        [
-        "v54.0",
-        0.1
-        ],
-        [
-        "v52.0",
-        0.95
-        ],
-        [
-        "v51.0",
-        0.15
-        ],
-        [
-        "v50.0",
-        0.1
-        ],
-        [
-        "v48.0",
-        0.31
-        ],
-        [
-        "v47.0",
-        0.12
-        ]
-        ]
-      },
-      {
-        name: "Internet Explorer",
-        id: "Internet Explorer",
-        data: [
-        [
-        "v11.0",
-        6.2
-        ],
-        [
-        "v10.0",
-        0.29
-        ],
-        [
-        "v9.0",
-        0.27
-        ],
-        [
-        "v8.0",
-        0.47
-        ]
-        ]
-      },
-      {
-        name: "Safari",
-        id: "Safari",
-        data: [
-        [
-        "v11.0",
-        3.39
-        ],
-        [
-        "v10.1",
-        0.96
-        ],
-        [
-        "v10.0",
-        0.36
-        ],
-        [
-        "v9.1",
-        0.54
-        ],
-        [
-        "v9.0",
-        0.13
-        ],
-        [
-        "v5.1",
-        0.2
-        ]
-        ]
-      },
-      {
-        name: "Edge",
-        id: "Edge",
-        data: [
-        [
-        "v16",
-        2.6
-        ],
-        [
-        "v15",
-        0.92
-        ],
-        [
-        "v14",
-        0.4
-        ],
-        [
-        "v13",
-        0.1
-        ]
-        ]
-      },
-      {
-        name: "Opera",
-        id: "Opera",
-        data: [
-        [
-        "v50.0",
-        0.96
-        ],
-        [
-        "v49.0",
-        0.82
-        ],
-        [
-        "v12.1",
-        0.14
-        ]
-        ]
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
       }
-      ]
     }
-  });
+  },
+  series: [{
+    name: 'Brands',
+    colorByPoint: true,
+    data: [{
+      name: 'Didisposisi',
+      y:  <?php echo json_encode($total_data_dis); ?>,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'Belum Didisposisi',
+      y:  <?php echo json_encode($total_data_sm); ?>
+    }]
+  }]
+});
 </script>
 
 <style type="text/css">
@@ -550,7 +324,7 @@
   .highcharts-figure,
   .highcharts-data-table table {
     min-width: 320px;
-    max-width: 660px;
+    max-width: 800px;
     margin: 1em auto;
   }
 
@@ -588,5 +362,9 @@
 
   .highcharts-data-table tr:hover {
     background: #f1f7ff;
+  }
+
+  input[type="number"] {
+    min-width: 50px;
   }
 </style>
